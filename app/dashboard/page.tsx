c9a0d1d5332587d53a89bbef64cf3d13935f3a23
Roger Graham <rogergraham147@gmail.com>
2024-09-17 22:20:24 +0300

    Set container width

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,8 +8,8 @@ export default async function Page() {
       defaultOpen={cookies().get("sidebar:state")?.value === "true"}
     >
       <AppSidebar />
-      <main className="flex flex-1 flex-col p-2 transition-all duration-300 ease-in-out">
-        <div className="h-full rounded-md border-2 border-dashed p-2">
+      <main className="flex flex-1 flex-col justify-center items-center p-2 transition-all duration-300 ease-in-out ">
+        <div className="h-full w-full max-w-[80rem] rounded-md border-2 border-dashed p-2">
           <SidebarTrigger />
           <Dashboard />
         </div>
