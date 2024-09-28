8d38f18307ffc2e01fa1f08071553eccdbbfcc67
Roger Graham <rogergraham147@gmail.com>
2024-09-29 00:08:21 +0300

    remove unused head import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Head from "next/head";
 import "./globals.css";
 import ThemeContextProvider from "@/context/theme-switch";
 import { getServerSession } from "next-auth/next";
