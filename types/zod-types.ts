e15b6a759212c5461e8b3d719829f33efe2e4926
Roger Graham <rogergraham147@gmail.com>
2024-10-02 14:32:06 +0300

    Refine validation to prevent more than 2 decimal places in price

--- types/zod-types.ts
+++ types/zod-types.ts
@@ -3,11 +3,14 @@ import { z } from "zod";
 export const stockFormSchema = z.object({
   symbol: z.string().min(1, "Stock symbol is required"),
   name: z.string().min(1, "Stock name is required"),
-  quantity: z.number().int().positive("Quantity must be a positive integer"),
+  quantity: z.number().int().positive("Quantity must be a positive number"),
   avgPrice: z
     .number()
     .positive("Average price must be a positive number")
-    .multipleOf(0.01, "Price must have at most 2 decimal places"),
+    // multiply by 100 to ensure it results in an integer, not more than two decimal places
+    .refine((val) => Number.isInteger(val * 100), {
+      message: "Price cannot have more than 2 decimal places",
+    }),
 });
 
 export type StockFormData = z.infer<typeof stockFormSchema>;
