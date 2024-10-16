6d0743c440bf3da15fa56869276e5b768903e2d9
Roger Graham <rogergraham147@gmail.com>
2024-10-16 15:14:14 +0300

    Add welcome text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 //use async function as getServerSession is promise
 export default async function Home() {
@@ -8,10 +9,17 @@ export default async function Home() {
   if (!session) {
     redirect("/auth/signin");
   }
-if (session?.user) {
-    redirect("/dashboard");
-  }
-
-
-  return null
+  return (
+    <div className="flex flex-col items-center justify-start min-h-screen py-2 mt-8">
+      <h1>Hello {session?.user?.name}! </h1>
+      <p>
+        {" "}
+        Please visit your{" "}
+        <Link className="underline" href="/dashboard">
+          Dashboard
+        </Link>{" "}
+        to see your portfolio
+      </p>
+    </div>
+  );
 }
